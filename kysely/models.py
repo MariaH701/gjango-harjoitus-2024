daffbe1ed6729d03a2bdc57e448a46943497b104
MariaH701 <51482@edu.raseko.fi>
2024-02-05 09:56:04 +0200

    Lisää kysely -appiin  etusivu

--- kysely/models.py
+++ kysely/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Kysymys(models.Model):
+    teksti = models.CharField(max_length=200)
+    julkaisupvm = models.DateTimeField("julkaistu")
+
+    def __str__(self):
+        return self.teksti
+
+
+class Vaihtoehto(models.Model):
+    kysymys = models.ForeignKey(Kysymys, on_delete=models.CASCADE)
+    teksti = models.CharField(max_length=200)
+    ääniä = models.IntegerField(default=0)
