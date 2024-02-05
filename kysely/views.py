abbdafd7be1f4c6bfdbcfd20c38dc72652966223
MariaH701 <51482@edu.raseko.fi>
2024-02-05 13:38:35 +0200

    Päivitä nämä kysely appin etusivulle

--- kysely/views.py
+++ kysely/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse 
+from django.shortcuts import get_object_or_404, render
 
 from .models import Kysymys
 
 def indeksi(request):
-
     kysymyslista = Kysymys.objects.order_by("-julkaisupvm")[:2]
     context = {
         "kysymykset": kysymyslista,
     }
     return render(request, "kysely/indeksi.html", context)    
 
-def näytä(request, question_id):
-    return HttpResponse("Katsot juuri kysymystä {question_id}")
+def näytä(request, kysymys_id):
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    return render(request, "kysely/näytä.html", {"kysymys": kysymys})
+    
 
 def tulokset(request, question_id):
     return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia.")
     
 def äänestä(request, question_id):
-    return HttpResponse("Olet äänestämässä kysymykseen {question_id}")
+    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
